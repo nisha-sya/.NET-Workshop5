b353192f83d6fe6e27a1b89d623327141a562b5c
nisha-sya <netramayathapamagar93@gmail.com>
2025-11-26 11:56:16 +0545

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc = new BankAccount("12345", 5000);
+            Console.WriteLine("Account Number: " + acc.AccountNumber);
+
+            acc.Deposit(2000);
+            acc.Withdraw(1000);
+
+            Console.WriteLine("Remaining Balance: " + acc.Balance);
+
+        }
+    }
+}
