42bd62b99d974c56e9ae9694ea95ab0a4a84ff54
nisha-sya <netramayathapamagar93@gmail.com>
2025-11-26 12:12:17 +0545

    Task2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car() { Brand = "Toyota", Speed = 120, Seats = 5 };
+            Motorcycle m = new Motorcycle() { Brand = "Honda", Speed = 90, Type = "Sports" };
+
+            c.Start();
+            c.DisplayInfo();
+            c.Stop();
+
+            m.Start();
+            m.DisplayInfo();
+            m.Stop();
+
+        }
+    }
+}
---
+++ Task2/Vechile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public int Speed { get; set; }
+
+        public void Start() => Console.WriteLine("Vehicle started");
+        public void Stop() => Console.WriteLine("Vehicle stopped");
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Speed: {Speed}");
+        }
+    }
+}
