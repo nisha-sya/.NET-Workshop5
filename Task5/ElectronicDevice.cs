de168c76bcf34e0895e2b82030a8af502640e71f
nisha-sya <netramayathapamagar93@gmail.com>
2025-11-26 21:01:58 +0545

    Task5 completed

---
+++ Task5/ElectronicDevice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public abstract class ElectronicDevice
+    {
+        private string brand;
+        private double price;
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public ElectronicDevice(string brand, double price)
+        {
+            Brand = brand;
+            Price = price;
+        }
+
+        public abstract void ShowInfo();
+    }
+
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ElectronicsStore store = new ElectronicsStore();
+
+            Laptop l1 = new Laptop("Dell", 1200);
+            Smartphone s1 = new Smartphone("Samsung", 900);
+
+            store.AddDevice(l1);
+            store.AddDevice(s1);
+
+            store.ShowAllDeviceDetails();
+
+        }
+    }
+}
