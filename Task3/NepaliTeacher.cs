8ab0911bcae9403da0bd3e51e5f7889c378f7d72
nisha-sya <netramayathapamagar93@gmail.com>
2025-11-26 20:33:47 +0545

    Task3 completed

---
+++ Task3/NepaliTeacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class NepaliTeacher : Teacher
+    {
+        public override void Teaching()
+        {
+            Console.WriteLine("Teacher teaches in Nepali");
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher nep = new NepaliTeacher();
+            nep.Teaching();
+            nep.SalaryInfo();
+
+            Teacher eng = new EnglishTeacher();
+            eng.Teaching();
+            eng.SalaryInfo();
+
+        }
+    }
+}
