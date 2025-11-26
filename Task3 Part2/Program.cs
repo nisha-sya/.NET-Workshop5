d8864900a36ae8ba2849a4c4daedbb866c455962
nisha-sya <netramayathapamagar93@gmail.com>
2025-11-26 21:19:59 +0545

    Added Task3 Part2

---
+++ Task3 Part2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task3_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer();
+            p.Print("Hello");
+            p.Print(123);
+            p.Print("Repeat", 3);
+        }
+    }
+}
